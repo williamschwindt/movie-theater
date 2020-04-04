78965124826c36d46b566d2b81a7cd14b795e52f
William Schwindt <williammschwindt@gmail.com>
2020-04-04 13:07:19 -0700

    cast action and reducer

---
+++ movie-theater/src/actions/movieActions/getMovieCast.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+import { GET_MOVIE_CAST_START, GET_MOVIE_CAST_SUCCESS, GET_MOVIE_CAST_FAILURE } from '../types';
+
+export const getMovieCast = (id) => dispatch => {
+    dispatch({ type: GET_MOVIE_CAST_START });
+
+    axios
+    .get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=f45d181e4568e696ff8f68048d522dc8`)
+    .then(res => {
+        console.log(res);
+        dispatch({ type: GET_MOVIE_CAST_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+        dispatch({ type: GET_MOVIE_CAST_FAILURE, payload: err });
+    })
+}
---
+++ movie-theater/src/reducers/movieReducers/movieCastReducer.js
@@ -0,0 +1,31 @@
+import { GET_MOVIE_CAST_START, GET_MOVIE_CAST_SUCCESS, GET_MOVIE_CAST_FAILURE } from '../../actions/types';
+
+const initialState = {
+    cast: {},
+    isFetching: false,
+    error: ''
+}
+
+export const movieDetailsRuducer = (state = initialState, action) => {
+    switch(action.type) {
+        case GET_MOVIE_CAST_START :
+            return {
+                ...state,
+                isFetching: true
+            }
+        case GET_MOVIE_CAST_SUCCESS :
+            return {
+                ...state,
+                cast: action.payload,
+                isFetching: 'fetched'
+            }
+        case GET_MOVIE_CAST_FAILURE :
+            return {
+                ...state,
+                isFetching: false,
+                error: action.payload
+            }
+        default :
+            return state;
+    }
+}
