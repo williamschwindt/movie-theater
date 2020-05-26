9d30288037328d4e186320101b990273f877f7de
William Schwindt <williammschwindt@gmail.com>
2020-05-26 14:48:41 -0700

    nav fully responsive

--- movie-theater/src/components/navbar/NavBar.js
+++ movie-theater/src/components/navbar/NavBar.js
@@ -7,20 +7,46 @@ const NavBar = () => {
         setQuery(e.target.value);
     }
 
+    const showNav = () => {
+        document.querySelector('.nav-links-hidden').style.display = 'none';
+        document.querySelector('.nav-links-close').style.display = 'flex';
+        document.querySelector('.nav-links-shown').style.display = 'flex';
+    }
+
+    const closeNav = () => {
+        document.querySelector('.nav-links-hidden').style.display = 'flex';
+        document.querySelector('.nav-links-close').style.display = 'none';
+        document.querySelector('.nav-links-shown').style.display = 'none';
+    }
+
+
     return (
-        <nav className="nav">
-            <div className="logo">
-                <h1>The Film House</h1>
-                <ion-icon className="film-icon" name="ios-film"/>
-            </div>
-            <input onChange={changeHandler} placeholder="search" />
-            <a className ="search-btn" href={`/search/${query}`}><ion-icon name="ios-search"/></a>
-            <div className="nav-links">
+        <div>
+            <nav className="nav">
+                <div className="logo">
+                    <h1>The Film House</h1>
+                    <ion-icon className="film-icon" name="ios-film"/>
+                </div>
+                <input onChange={changeHandler} placeholder="search" />
+                <a className ="search-btn" href={`/search/${query}`}><ion-icon name="ios-search"/></a>
+                <div className="nav-links-hidden">
+                    <ion-icon onClick={showNav} className="hidden-icon" name="ios-menu" />
+                </div>
+                <div className="nav-links-close">
+                    <ion-icon onClick={closeNav} className="close-icon" name="ios-close" />
+                </div>
+                <div className="nav-links">
+                    <a href="/">Home</a>
+                    <a href="/discover">Discover</a>
+                    <a href="/login">Login</a>
+                </div>
+            </nav>
+            <div className="nav-links-shown">
                 <a href="/">Home</a>
                 <a href="/discover">Discover</a>
                 <a href="/login">Login</a>
             </div>
-        </nav>
+        </div>
     )
 }
 
