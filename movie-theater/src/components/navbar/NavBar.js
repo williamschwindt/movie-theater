2242b3301459cf3d7e76619b55b2065bc4555f30
William Schwindt <williammschwindt@gmail.com>
2020-05-26 15:07:41 -0700

    fixed search button on small screens

--- movie-theater/src/components/navbar/NavBar.js
+++ movie-theater/src/components/navbar/NavBar.js
@@ -27,8 +27,10 @@ const NavBar = () => {
                     <h1>The Film House</h1>
                     <ion-icon className="film-icon" name="ios-film"/>
                 </div>
-                <input onChange={changeHandler} placeholder="search" />
-                <a className ="search-btn" href={`/search/${query}`}><ion-icon name="ios-search"/></a>
+                <div className="search-container">
+                    <input onChange={changeHandler} placeholder="search" />
+                    <a className ="search-btn" href={`/search/${query}`}><ion-icon name="ios-search"/></a>
+                </div>
                 <div className="nav-links-hidden">
                     <ion-icon onClick={showNav} className="hidden-icon" name="ios-menu" />
                 </div>
