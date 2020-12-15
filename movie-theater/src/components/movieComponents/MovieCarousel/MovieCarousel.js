4b843fba994917b45c3ad091bc46538d622adc5c
William Schwindt <williammschwindt@gmail.com>
2020-12-15 13:13:14 -0800

    updated movie carousel sliding

--- movie-theater/src/components/movieComponents/MovieCarousel/MovieCarousel.js
+++ movie-theater/src/components/movieComponents/MovieCarousel/MovieCarousel.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export const MovieCarousel = (props) => {
@@ -10,30 +10,22 @@ export const MovieCarousel = (props) => {
         return text;
     }
 
-    const slide = (amount) => {
-        const row = document.querySelectorAll(`.${props.class}`);
-        for(let i = 0; i < row.length; i++) { 
-            row[i].style.transform = `translateX(${amount}%)`;
-        }
-    }
+    const [slideAmount, setSlideAmount] = useState(0);
 
-    let transform = 0;
     const next = () => {
-        if(transform > -1513.2) {
-            transform -= 116.4;
+        if(slideAmount > -1513.2) {
+            setSlideAmount(slideAmount - 116.4);
         } else {
-            transform = 0;
+            setSlideAmount(0)
         }
-        slide(transform);
     }
 
     const prev = () => {
-        if(transform > -.01 && transform < .01) {
-            transform = -1513.2;
+        if(slideAmount > -.01 && slideAmount < .01) {
+            setSlideAmount(-1513.2);
         } else {
-            transform += 116.4;
+            setSlideAmount(slideAmount + 116.4);
         }
-        slide(transform);
     }
 
     return (
@@ -41,17 +33,15 @@ export const MovieCarousel = (props) => {
             <div className="movies-row">
                 {props.movies.map(movie => {
                     return (
-                        <Link to={`/movie/${movie.id}`}  key={movie.id} className={props.class}>
+                        <Link to={`/movie/${movie.id}`}  key={movie.id} className={props.class} style={{ transform: `translateX(${slideAmount}%)` }}>
                             <img src={`${props.config}w1280${movie.poster_path}`} alt={movie.title}/>
                             <h2>{shortendText(movie.title, 40)}</h2>
                         </Link>
                     )
                 })}
             </div>
-            
             <button className="back c" onClick={prev}><ion-icon name="ios-arrow-back"/></button>
             <button className="next c" onClick={next}><ion-icon name="ios-arrow-forward"/></button>
-        
         </div>
     )
 }
